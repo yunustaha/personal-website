eb0a4cf1492d6ae63dd0f0aee4957e8b1ca1dba7
Yunus Taha Yılmaz <yunusbilge38@gmail.com>
2023-09-06 23:52:43 +0300

    added a project article to project page.

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -7,6 +7,12 @@ const Projects = () => {
       <h1 className='text-title text-4xl font-semibold '>Projeler</h1>
       <ProjectCard title={'Mutay Tech'} description={'Next.js ve Tailwind CSS ile Formik, Leaflet, Axios ve React-Toastify paketlerini kullanarak Mutay Tech isimli marka için geliştirdiğim tanıtım websitesi projesi.'} imgUrls={['/projects/mutay-home.png', '/projects/mutay-about.png', '/projects/mutay-contact.png']} liveUrl={'https://mutaytech.com.tr/'} />
       <ProjectCard title={'Avanos Bilgisayar'} description={'Next.js ve Tailwind CSS ile Avanos Bilgisayar isimli marka için geliştirdiğim tanıtım websitesi projesi.'} imgUrls={['/projects/avanosbilgisayar-home.png', '/projects/avanosbilgisayar-contact.png']} liveUrl={'http://avanosbilgisayar.com/'} />
+      <ProjectCard
+        title={'Sport With AI'}
+        description={`Bir tez projesi olarak üç kişilik bir ekip ile geliştirilmiş bir spor uygulaması olan Sport With AI'ı tasarladık ve kodladık. Bu uygulama, egzersiz hareketlerini görüntü işleme yoluyla analiz ediyor. Frontend için Angular, backend için NodeJS, mobil geliştirme için Flutter ve yapay zeka için Python kullanıldı. Sosyal medya bölümü için fotoğraf yükleme (EC2), metin paylaşımı (MongoDB) ve önbellek yapısı (Redis) uygulandı.`}
+        imgUrls={['/projects/sportwithai.webp']}
+        gitHubUrl='https://github.com/yunustaha/sport-with-ai-monorepo'
+      />
     </main>
   )
 }
